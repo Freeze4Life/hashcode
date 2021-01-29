64ce81e488547f0369eafe98d25033ccb18b6aae
Mohammed Afraaz <mohammedpsvita@gmail.com>
2021-01-30 01:20:30 +0530

    Fixed errors Added Library value calc logic

--- qual2020/main.py
+++ qual2020/main.py
@@ -1,3 +1,5 @@
+import operator
+
 class Library:
     def __init__(self,id,n_books,time,rate,books):
         self.id = id
@@ -5,12 +7,19 @@ class Library:
         self.time = time
         self.rate = rate
         self.books = books
-
+        self.score = 0
+    def __str__(self):
+        return f"ID:{self.id}"
+    def arrange_books(self):
+        self.books=[ele for ele in self.books if ele not in comp_books]
+        self.books=sorted(self.books,reverse=True,key=(lambda x: x.score))
 
 class Book:
     def __init__(self,id,score):
         self.id = id
         self.score = score
+    def __str__(self):
+        return f"ID:{self.id} SCORE:{self.score}"
 
 TIME = int()
 
@@ -18,17 +27,24 @@ TIME = int()
 B,L,D = tuple(map(int,input().split()))
 TIME = D
 scores = list(map(int,input().split()))
-books = list(B)
-libraries = list(L)
+books = list()
+libraries = list()
 for i in range(B):
-    books[i] = Book(i,scores[i])
+    books.append(Book(i,scores[i]))
 
 for i in range(L):
     N,T,M = tuple(map(int,input().split()))
     bookids = list(map(int,input().split()))
-    libraries[i] = Library(i,N,T,M,bookids)
+    lib_books = [books[i] for i in bookids]
+    libraries.append(Library(i,N,T,M,lib_books))
 
 #LOGIC
 # give each lib a rank decided by total score of that library divided by no of days it takes to sign up
 # k = (rate * no of days left)
 # score = (score of top k books without repetitions)
+comp_books = list()
+for time in range(D,0,-1):
+    for lib in libraries:
+        lib.arrange_books()
+        quant = (time-lib.time)*lib.rate
+        lib.value = sum([temp.score for temp in lib.books[:quant]])/lib.time
