1f31c3365b5bdb8627f3ff0b3657bd626aef2d75
Jugal <jugalpatel100@gmail.com>
2021-01-30 19:02:05 +0530

    updated input statements and added a method to sort book indices list of a library

--- qual2020/main.py
+++ qual2020/main.py
@@ -1,34 +1,76 @@
+#NOTE classes
+
 class Library:
-    def __init__(self,id,n_books,time,rate,books):
+
+    book_scores = list()
+
+    def __init__(self, id, n_books, time, rate, books_id_arr):
         self.id = id
         self.n_books = n_books
         self.time = time
         self.rate = rate
-        self.books = books
+        self.books_id_arr = books_id_arr
+    
 
 
-class Book:
-    def __init__(self,id,score):
-        self.id = id
-        self.score = score
+    def worth(self, days_rem):
+        # sum of scores of books for (rem days - signup time) indices / signup time
+        return sum(list(map(lambda book_id: Library.book_scores[book_id], self.books_id_arr[ : (days_rem-self.time) ]))) / self.time
 
-TIME = int()
 
-#INPUT
-B,L,D = tuple(map(int,input().split()))
-TIME = D
-scores = list(map(int,input().split()))
-books = list(B)
-libraries = list(L)
-for i in range(B):
-    books[i] = Book(i,scores[i])
+# class Book:
+#     def __init__(self, id, score):
+#         self.id = id
+#         self.score = score
 
-for i in range(L):
-    N,T,M = tuple(map(int,input().split()))
-    bookids = list(map(int,input().split()))
-    libraries[i] = Library(i,N,T,M,bookids)
+# TIME = int()
 
-#LOGIC
+
+
+
+#NOTE Functions
+
+def sort_book_indices(book_ids):
+        books_dict = dict()
+
+        for id in book_ids:
+            books_dict[id] = Library.book_scores[id]
+        
+        #sort based on scores
+        sorted_books_dict = dict(sorted(books_dict.items(), key=lambda item: item[1], reverse=True))
+
+        return list(sorted_books_dict.keys())
+
+
+
+def main():
+    # INPUT
+
+    B, L, D = tuple(map(int, input().split()))
+
+    Library.book_scores = list(map(int, input().split()))
+
+    libraries = list()
+
+    for i in range(L):
+        no_of_books, signup_time, max_books_per_day = tuple(map(int, input().split()))
+
+        book_ids = list(map(int, input().split()))
+        sorted_book_ids = sort_book_indices(book_ids)
+        
+        libraries.append(Library(i, no_of_books, signup_time,max_books_per_day, sorted_book_ids))
+
+
+    print(libraries[1].books_id_arr)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+# LOGIC
 # give each lib a rank decided by total score of that library divided by no of days it takes to sign up -> worth
 # k = (rate * no of days left)
 # score = (score of top k books without repetitions)
